29bf8d4a664ef9f97455cef85df14c37bcfa1890
Stephen Mogensen <sdmogensen@mac.local>
2016-09-20 21:07:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# P2: XKCD Password Generator
+
+## Live URL
+<http://p2.mogensen.site/>
+
+## Description
+P2
+
+## Demo
+<>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Clock</title>
+    <meta charset='utf-8'>
+
+    <link rel='stylesheet' href='styles.css' type='text/css'>
+
+    <?php require('logic.php'); ?>
+
+</head>
+<body>
+
+</body>
+</html>
